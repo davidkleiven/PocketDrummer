3a29872119026019fd8e8de414c9149f64cb7091
David Kleiven <davidkleiven446@gmail.com>
2020-08-30 10:34:22 +0200

    ENH: Add empty service file

--- index.js
+++ index.js
@@ -8,6 +8,4 @@ import {name as appName} from './app.json';
 import TrackPlayer from 'react-native-track-player';
 
 AppRegistry.registerComponent(appName, () => App);
-// TODO: Look into this. Think event listeners should be added here
-// But this line silences the "no tasks registered warning"
-TrackPlayer.registerPlaybackService(() => async (data) =>{})
+TrackPlayer.registerPlaybackService(() => require('./service'))
