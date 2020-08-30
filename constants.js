d07f11951bfaf3286657e939eb3a572c03a0eb18
David Kleiven <davidkleiven446@gmail.com>
2020-08-30 15:05:58 +0200

    ENH: Add demo track button that downloads demo from GDrive

--- constants.js
+++ constants.js
@@ -6,5 +6,6 @@ export const FILL_IDLE = 0
 export const FILL_REQUESTED = 1
 
 export const EXAMPLE_LINK = 'https://drive.google.com/drive/folders/1DKWbsj0MkGPuLBlNI9Op6xhiyt0_zJE0?usp=sharing'
+export const DEMO_TRACK = 'https://drive.google.com/uc?export=download&id=1olLythElmS-bHNocPQcZOuf59E9B34KD'
 export const SUPPORT_EMAIL = 'davidkleiven446@gmail.com'
 export const WEBPAGE = 'https://github.com/davidkleiven/RhythmiFix'
